f01e8b71a5dc216bd61427161d0420b368dd67a7
Bogdan Preda <bogdanpreda@Bogdans-iMac.local>
2016-08-11 16:16:40 +0300

    update2

--- tracking_service/assets/app2.js
+++ tracking_service/assets/app2.js
@@ -1,4 +1,4 @@
-/*
+/*car driver
 function initMap() {
     // Initialize the Google Maps API v3
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -35,24 +35,28 @@ function initMap() {
 
     autoUpdate();
 }
-*/
 
-var lat = 42.756432;
+*/
+var lat = 45.756432;
 var lng = 21.228477;
 var position = {lat, lng};
+var poli;
+
+
 
 
 console.log(position);
       function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: position,
-          zoom: 18,
+          zoom: 2,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         });
 
+        var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
         var infoWindow = new google.maps.InfoWindow({map: map});
 
-
+        var bound = new google.maps.LatLngBounds();
 
         var map_marker = new google.maps.Marker({
             map: map,
@@ -65,40 +69,38 @@ console.log(position);
 
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
 
-            var marker = null;
 
-            function autoUpdate() {
-              navigator.geolocation.getCurrentPosition(function(position) {
+            var path = [];
+            path.push(new google.maps.LatLng(pos.lat, pos.lng));
 
-                alert(position.coords.latitude);
+            var polyline = new google.maps.Polyline({
+            map: map,
+            path: path,
+            strokeColor: '#0000FF',
+            strokeOpacity: 0.7,
+            strokeWeight: 1
+          });
 
-                var newPoint = new google.maps.LatLng(position.coords.latitude,
-                                                      position.coords.longitude);
+            //marker.setPosition(new google.maps.LatLng(pos.lat, pos.lng));
+            map_marker.setPosition(pos);
+            map_marker.setMap(map);
+            map.setZoom(17);
+            map.setCenter(pos);
+            var i=0;
 
-                if (marker) {
-                  // Marker already created - Move it
-                  marker.setPosition(newPoint);
-                }
-                else {
-                  // Marker does not exist - Create it
-                  marker = new google.maps.Marker({
-                    position: newPoint,
-                    map: map
-                  });
-                }
+            setInterval(function() {
+                pos.lat -=0.00007;
+                pos.lng-=0.000007;
 
-                // Center the map on the new position
-                map.setCenter(newPoint);
-              });
+                redraw(pos);
 
-              // Call the autoUpdate() function every 5 seconds
-              setTimeout(autoUpdate, 5000);
-            }
-
-            autoUpdate();
-
+            },550);
 
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
@@ -119,7 +121,7 @@ console.log(position);
             //map.setCenter({lat: pos.lat, lng : pos.lng, alt: 0})
             map.panTo(pos);
             map_marker.setPosition({lat: pos.lat, lng : pos.lng, alt: 0});
-
+            path.push(new google.maps.LatLng(pos.lat, pos.lng));
         }
 
       }
